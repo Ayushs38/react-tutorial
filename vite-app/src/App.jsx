7d1709ea1ea90729bc679fc524de03b51be00dae
Ayushs38 <ayushsakhale98@gmail.com>
2024-07-25 21:46:52 +0530

    class 2- mapping of list

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -4,37 +4,15 @@ import Home from './components/home/home/Home'
 import Button from 'react-bootstrap/Button';
 
 
-function handleClick() {
-  alert("helloee everone!")
-}
-
-function MyButton() {
-
-  return (
-    <>
-      
-
-      <button onClick={handleClick} className='btn btn-primary'>
-        I'm a button
-      </button>
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
-      <Button variant="link">Link</Button>
-    </>
-
-
-  );
-}
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home />
-      <MyButton />
-      <footer></footer>
+      {/* <Home />
+      <MyButton /> */}
+      {/* <footer></footer> */}
     </>
   )
 }
--- vite-app/src/components/header/Header.jsx
+++ vite-app/src/components/header/Header.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
+
+  const NavItems = [
+    {
+      link: "#",
+      name: "Home",
+     
+    },
+    {
+      link: "#",
+      name: "About Us"
+    },
+    {
+      link: "#",
+      name: "Service"
+    },
+    {
+      link: "#",
+      name: "Gallery"
+    },
+    {
+      link: "#",
+      name: "Contact"
+    },
+  ];
+
   return (
-    <div>Header</div>
-  )
+    <>
+      <div>
+        <span>Logo</span>
+        <nav>
+          <ul >
+            {
+            NavItems.map((janvi) => (
+              <li>
+                <a href={janvi.link}>{janvi.name}</a>
+              </li>
+            ))
+          }
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
